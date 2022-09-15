276d471b24124434977bc315ea5c984725ac15ec
Alvaro Codina <acodina@idealista.com>
2022-09-15 16:53:36 +0200

    add Scenario and ScenarioTest

---
+++ src/Game/Scenario/Scenario.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Game\Scenario;
+
+use App\Game\Zombie\Zombie;
+use App\Game\Survivor\Survivor;
+use Doctrine\Common\Collections\ArrayCollection;
+use RuntimeException;
+
+final class Scenario
+{
+    /** @var ArrayCollection<Survivor> $survivors */
+    private ArrayCollection $survivors;
+
+    /** @var ArrayCollection<Zombie> $zombies */
+    private ArrayCollection $zombies;
+
+    /** @var ArrayCollection<Item> $items */
+    private ArrayCollection $items;
+
+    /**
+     * @param int $size
+     */
+    public function __construct(
+        private int $size
+    ) {
+        if (3 > $this->size) {
+            throw new RuntimeException('Scenario size too low');
+        }
+
+        $this->survivors = new ArrayCollection();
+        $this->zombies = new ArrayCollection();
+        $this->items = new ArrayCollection();
+    }
+
+    public function getSize(): int
+    {
+        return $this->size;
+    }
+
+    /**
+     * @return ArrayCollection<Survivor>
+     */
+    public function getSurvivors(): ArrayCollection
+    {
+        return $this->survivors;
+    }
+
+    /**
+     * @return ArrayCollection<Zombie>
+     */
+    public function getZombies(): ArrayCollection
+    {
+        return $this->zombies;
+    }
+
+    /**
+     * @return ArrayCollection<Items>
+     */
+    public function getItems(): ArrayCollection
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param Survivor $survivor
+     * @return Scenario
+     */
+    public function addSurvivor(Survivor $survivor): self
+    {
+        $this->survivors->add($survivor);
+
+        return $this;
+    }
+
+    /**
+     * @param Zombie $zombie
+     * @return Scenario
+     */
+    public function addZombie(Zombie $zombie): self
+    {
+        $this->zombies->add($zombie);
+
+        return $this;
+    }
+
+    /**
+     * @param Item $item
+     * @return Scenario
+     */
+    public function addItem(Item $item): self
+    {
+        $this->items->add($item);
+
+        return $this;
+    }
+}
