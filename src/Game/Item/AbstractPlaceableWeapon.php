7258b0b89d30ac30afac9b5671c4c4e8e1e57d02
Victor Vispe <victor.vispe@avaibooksports.com>
2022-09-15 17:36:13 +0200

    Crea usos de arma

--- src/Game/Item/AbstractPlaceableWeapon.php
+++ src/Game/Item/AbstractPlaceableWeapon.php
@@ -9,26 +9,5 @@ use App\Game\Item\Exception\WeaponIsNotUsableException;
 
 abstract class AbstractPlaceableWeapon implements SurvivorWeaponInterface
 {
-    private function __construct(public ?Vector2 $vector2)
-        {
-        }
 
-        public static function createInMap(Vector2 $vector2): static
-        {
-            return new static($vector2);
-        }
-
-        public static function createInSurvivor(): static
-        {
-            return new static(null);
-        }
-
-        public function use(): void
-        {
-            if (null === $this->vector2) {
-
-            }
-
-            throw new WeaponIsNotUsableException();
-        }
 }
--- src/Game/Item/BaseballBat.php
+++ src/Game/Item/BaseballBat.php
@@ -4,8 +4,28 @@ declare(strict_types=1);
 
 namespace App\Game\Item;
 
-class BaseballBat extends AbstractPlaceableWeapon
+use App\Game\Item\Exception\WeaponIsNotUsableException;
+use App\Shared\Vector2;
+
+class BaseballBat extends AbstractPlaceableWeapon implements ExpendableWeaponInterface
 {
+    public Uses $uses;
+
+    private function __construct(public ?Vector2 $vector2)
+    {
+         $this->uses = new Uses(1);
+    }
+
+    public static function createInMap(Vector2 $vector2): static
+    {
+        return new static($vector2);
+    }
+
+    public static function createInSurvivor(): static
+    {
+        return new static(null);
+    }
+
     public function getName(): string
     {
         return 'Baseball Bat';
@@ -21,8 +41,22 @@ class BaseballBat extends AbstractPlaceableWeapon
         return 1;
     }
 
-    public function getUses(): int
+    public function use(): void
     {
-        return 1;
+        if (null !== $this->vector2 || !$this->isWasted()) {
+            throw new WeaponIsNotUsableException();
+        }
+
+        $this->spendUse();
+    }
+
+    private function isWasted(): bool
+    {
+        return $this->uses->uses <= 0;
+    }
+
+    private function spendUse():void
+    {
+        $this->uses = new Uses(($this->uses->uses)-1);
     }
 }
